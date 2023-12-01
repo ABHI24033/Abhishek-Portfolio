2cecfce7d4fe6b368b320e5dbb69c307342a7469
Abhishek Kumar <abhi24033c@gmail.com>
2023-12-02 02:42:13 +0530

    make navbar active

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,35 @@ bars.addEventListener("click",()=>{
     bars.classList.toggle("fa-times");
     header.classList.toggle("toggle");
 })
+
+// activete the current navbar
+
+let list=document.querySelectorAll(".navbar ul li>a");
+console.log(list);
+const section=document.querySelectorAll(".section");
+
+function activeLink(li) {
+    list.forEach((item) => item.classList.remove('active'));
+    li.classList.add('active');
+}
+list.forEach((item)=>{
+    item.addEventListener("click",()=>{
+        activeLink(item);
+        bars.classList.toggle("fa-times");
+        header.classList.remove("toggle");
+    })
+})
+
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            const target = document.querySelector(`[href='#${id}']`);
+            activeLink(target);
+        }
+    })
+};
